06f47866b8320a6f73f6e4086a78f69092a3b856
plusdojo2026-urushihara <urushihara-yuto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 08:50:41 +0900

    adminloginservlet6/17

--- source/src/main/java/servlet/AdminLoginServlet.java
+++ source/src/main/java/servlet/AdminLoginServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,9 @@ public class AdminLoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 管理者ログイン画面に移る
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/adminlogin.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
