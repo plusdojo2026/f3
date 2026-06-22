85c93c7c449736d2a7ddfe0309fe6bef1851f74c
plusdojo2026-urushihara <urushihara-yuto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 12:03:51 +0900

    漆原担当箇所12:03

--- source/src/main/java/dao/AdminDao.java
+++ source/src/main/java/dao/AdminDao.java
@@ -1,5 +1,51 @@
 package dao;
 
-public class AdminDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-}
+import dto.Admin;
+
+public class AdminDao {
+	public Admin login(String admin_id, String password ) {
+		Connection conn = null;
+		Admin admin = null;
+		
+		try {
+			// JDBCドライバを読み込む
+						Class.forName("com.mysql.cj.jdbc.Driver");
+
+						// データベースに接続する
+						conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f3?"
+								+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+								"root", "password");
+						String sql = "SELECT admin_id,pssword FROM admin WHERE admin_id = ? AND password = ?";
+						PreparedStatement pStmt = conn.prepareStatement(sql);
+						pStmt.setString(1, admin_id);
+						pStmt.setString(2, password);
+						
+						// SELECT文を実行し、結果表を取得する
+						ResultSet rs = pStmt.executeQuery();
+						// ユーザーIDとパスワードが一致するユーザーがいれば結果をtrueにする
+						if (rs.next()) {
+			                admin = new Admin();  // ★ user_id を返す
+			                admin.setAdmin_id(rs.getInt("admin_id"));
+			                admin.setPassword(rs.getString("password"));
+			            }
+
+			        } catch (Exception e) {
+			            e.printStackTrace();
+			        } finally {
+			            if (conn != null) try { conn.close(); } catch (SQLException e) {}
+			        }
+					
+		return admin;
+
+
+		}
+		
+	}
+
+
--- source/src/main/java/dto/Admin.java
+++ source/src/main/java/dto/Admin.java
@@ -1,5 +1,32 @@
 package dto;
 
-public class Admin {
+import java.io.Serializable;
+
+public class Admin implements Serializable{
+	private int admin_id;
+	private String admin_name;
+	private String password;
+	
+	public int getAdmin_id() {
+		return admin_id;
+	}
+	public void setAdmin_id(int admin_id) {
+		this.admin_id = admin_id;
+	}
+	
+	public String getAdmin_name() {
+		return admin_name;
+	}
+	public void setAdmin_name(String admin_name) {
+		this.admin_name = admin_name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 
 }
