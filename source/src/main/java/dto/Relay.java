5c33dda946c691be6cff45fe763036743652e96a
plusdojo2026-urushihara <urushihara-yuto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 17:31:13 +0900

    DTO

--- source/src/main/java/dto/Relay.java
+++ source/src/main/java/dto/Relay.java
@@ -1,5 +1,82 @@
 package dto;
 
+import java.time.LocalDateTime;
+
 public class Relay {
+	private int relay_id;
+	private String user_id;
+	private int process_count;
+	private int project_id;
+	private int redraw_count;
+	private String relay_image_url;
+	private LocalDateTime assigned_at;
+	private LocalDateTime deadline_at;
+	
+	public Relay() {
+		
+	}
+	public int getRelay_id() {
+		return relay_id;
+	}
+	public void setRelay_id(int relay_id) {
+		this.relay_id = relay_id;
+		
+	}
+	public int getRelay_id() {
+		return relay_id;
+	}
+	public void setRelay_id(int relay_id) {
+		this.relay_id = relay_id;
+		
+	}
+	public int getRelay_id() {
+		return relay_id;
+	}
+	public void setRelay_id(int relay_id) {
+		this.relay_id = relay_id;
+		
+	}
+	public int getRelay_id() {
+		return relay_id;
+	}
+	public void setRelay_id(int relay_id) {
+		this.relay_id = relay_id;
+		
+	}
+	public int getRelay_id() {
+		return relay_id;
+	}
+	public void setRelay_id(int relay_id) {
+		this.relay_id = relay_id;
+		
+	}
+	public int getRelay_id() {
+		return relay_id;
+	}
+	public void setRelay_id(int relay_id) {
+		this.relay_id = relay_id;
+		
+	}
+	public int getRelay_id() {
+		return relay_id;
+	}
+	public void setRelay_id(int relay_id) {
+		this.relay_id = relay_id;
+		
+	}
+	public int getRelay_id() {
+		return relay_id;
+	}
+	public void setRelay_id(int relay_id) {
+		this.relay_id = relay_id;
+		
+	}
+	public int getRelay_id() {
+		return relay_id;
+	}
+	public void setRelay_id(int relay_id) {
+		this.relay_id = relay_id;
+		
+	}
 
 }
