dfe244f46696049e8df7715407cb99d3fc39aed4
plusdojo2026-aberyomu <abe-ryomu-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 15:43:37 +0900

    Update Ban.java

--- source/src/main/java/dto/Ban.java
+++ source/src/main/java/dto/Ban.java
@@ -1,5 +1,33 @@
 package dto;
 
-public class Ban {
+import java.io.Serializable;
 
+public class Ban implements Serializable {
+	
+	private String userId;//対象ユーザー
+	private boolean ban;//BANされているかされていないか
+
+	public Ban() {
+	}
+	//値をセットして生成するコンストラクタ
+	public Ban(String userId, boolean ban) {
+		this.userId = userId;
+		this.ban = ban;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public boolean isBan() {
+		return ban;
+	}
+
+	public void setBan(boolean ban) {
+		this.ban = ban;
+	}
 }
