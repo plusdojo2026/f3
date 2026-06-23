044fdbaf2297bc982d3c330b389ad4068074468c
plusdojo2026-aberyomu <abe-ryomu-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-23 10:20:47 +0900

    Update HomeServlet.java

--- source/src/main/java/servlet/HomeServlet.java
+++ source/src/main/java/servlet/HomeServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.CurseDao;
+import dto.Curse;
 
 /**
  * Servlet implementation class HomeServlet
@@ -21,6 +25,24 @@ public class HomeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		// セッション取得
+		HttpSession session = request.getSession();
+		
+		// ログイン中のユーザーID取得
+		String userId =(String)session.getAttribute("userId");
+		
+		// CurseDAO生成
+		CurseDao dao = new CurseDao();
+		
+		// 呪い情報取得
+		Curse curse =dao.findByUserId(userId);
+		
+		// 呪いが存在したらJSPへ渡す
+				if(curse != null) {
+					request.setAttribute("curse",curse);
+				}
+		
 		// ホーム画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home.jsp");
 		dispatcher.forward(request, response);
